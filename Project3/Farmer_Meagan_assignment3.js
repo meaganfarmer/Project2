e86dfd71e3bc01e528701e82eb1d5ac8ca3d7ab9
Meagan Farmer <meaganfarmer@Meagans-MacBook-Pro.local>
2014-08-20 22:55:55 -0500

    testing

--- Project3/Farmer_Meagan_assignment3.js
+++ Project3/Farmer_Meagan_assignment3.js
@@ -1,1 +1,42 @@
 alert("JavaScript works!");
+//Meagan Farmer
+//SDI1408
+//Project 3
+//Arrays and JSON
+
+
+
+
+var cats = 0;                                   //global variables
+
+
+function myFunction(lives) {                    // math
+
+    math.floor(math.random()* 20);
+}
+
+
+//return value
+
+
+
+//while loop
+ 
+  while (cats < 0)
+   console.log( " Total cats are " + cats );
+        cats ++;
+        
+    
+
+//conditional
+
+var favorite = "all black cat";
+
+if (favorite) {
+
+greeting = "Yay, your my favorite!";
+
+} else {
+
+Console.log( "Dispose of this cat" + "!");
+}
---
+++ Project3/JSON data.js
@@ -0,0 +1,49 @@
+//jason
+//add script file to HTML
+//<script type="text/javascript" src="JSON data.js"></script>
+
+//JSON Data
+var jsonData = {
+    "cat names" : [
+   {  
+    "first name": ziggy 
+    "last name" : Tally
+    "number" :  1
+    },
+    {
+    "first name": Happy
+    "last name" : Jane
+    "number" : 2
+    },
+    {
+    "first name": liz
+    "last name" : bill
+    "number" : 3
+    },
+    {
+    "first name": Jandi
+    "last name" : Candi
+    "number" : 4
+    },
+    {
+    "first name": Pam
+    "last name" : Llama
+    "number" : 5
+    },
+    {
+    "first name": Kyric
+    "last name" : George
+    "number" : 6
+    },
+    {
+    "first name": Rach
+    "last name" :   Soda
+    "number" : 7
+    },
+    {
+    "first name": Linda
+    "last name" : beezwax
+    "number" : 8
+    }
+
+]}
